28142adef981a87e07791d2526762ce5f31ca09e
beaterblank <mohanteja.g2019@vitstudent.ac.in>
2021-02-09 00:11:34 +0530

    setupfile update

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
-
+with open("README.md","r") as fh:
+  long_description = fh.read()
 setup(
   name = 'asciigraphics',
   version = '0.0.1',
@@ -9,5 +10,7 @@ setup(
   classifiers = [
     "License :: GNU Genral public license v2 or later (GPLv2+)"
     "Operating System :: Microsoft :: Windows :: Windows 10"
-  ]
+  ],
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
   )
